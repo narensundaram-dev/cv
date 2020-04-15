f329276f0e42fe23282affec6a898ba9a26cbc92
Narendran Gnanasundaram <ngnanasu@cisco.com>
2020-04-15 19:42:53 +0530

    code-refactored

---
+++ utils/email_normalizer.py
@@ -0,0 +1,28 @@
+class EmailNormalizer(object):
+
+    def __init__(self, email):
+        self.email = email
+
+    def trim(self, return_=False):
+        new_string = ""
+        found = False
+        for s in self.email:
+            if not found:
+                if s.isalpha():
+                    new_string += s
+                    found = True
+            else:
+                new_string += s
+        if return_:
+            return new_string[::-1]
+        self.email = new_string[::-1]
+        return self.trim(True)
+
+    def normalize(self):
+        return self.trim()
+
+
+if __name__ == '__main__':
+    print(EmailNormalizer("../naren@gmail.com").normalize())
+    print(EmailNormalizer("naren@gmail.com12..,3").normalize())
+    print(EmailNormalizer("../naren@gmail.com12..,3").normalize())

d0e72416b7dc15c992716549cd1547ecb25d7cf1
Narendran Gnanasundaram <ngnanasu@cisco.com>
2020-04-15 19:53:31 +0530

    .

--- cv.py
+++ cv.py
@@ -3,10 +3,10 @@ import re
 import glob
 import json
 import shutil
-import traceback
 import zipfile
 import logging
 import argparse
+import traceback
 from io import BytesIO
 from datetime import datetime as dt
 
@@ -20,6 +20,8 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.converter import TextConverter
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 
+from utils.email_normalizer import EmailNormalizer
+
 
 __author__ = "Narendran G"
 __maintainer__ = "Narendran G"
@@ -28,8 +30,10 @@ __contact__ = "narensundaram007@gmail.com / +91 8678910063"
 log = logging.getLogger(__file__.split('/')[-1])
 
 # Loading all the available indian names
-indian_names = open("names.txt", "r").read().lower()
-indian_names = set(indian_names.split())
+path_indian_names = os.path.join(os.getcwd(), "setup", "names.csv")
+with open(path_indian_names, "r") as f:
+    series = pd.read_csv(f)
+    indian_names = series.drop_duplicates().values
 
 
 def config_logger(args):
@@ -59,6 +63,9 @@ class CVReader(object):
         self.initial_path = path
         self.path = path
         self.text = ""
+        self.text_path = ""
+        self.data = {}
+        self.skip = False
 
     @property
     def initial_filename(self):
@@ -98,13 +105,11 @@ class CVReader(object):
             tokens, lines, sentences = self.tokenize()
             grammar = r'NAME: {<NN.*><NN.*><NN.*>*}'
             parser = nltk.RegexpParser(grammar)
-            # all_chunked_tokens = []
             for tagged_tokens in lines:
                 if len(tagged_tokens) == 0:
                     continue
-                chunked_tokens = parser.parse(tagged_tokens)
-                # all_chunked_tokens.append(chunked_tokens)
-                for subtree in chunked_tokens.subtrees():
+                tokens_chunked = parser.parse(tagged_tokens)
+                for subtree in tokens_chunked.subtrees():
                     # if subtree.label() == 'NAME':
                     for idx, leaf in enumerate(subtree.leaves()):
                         # if leaf[0].lower() in indian_names and 'NN' in leaf[1]:
@@ -127,7 +132,8 @@ class CVReader(object):
         name = name.replace(".", " ")
         name = name.split(" ")[:3] if len(name.split(" ")) >= 3 else name.split(" ")
         for i, n in enumerate(name):
-            names[i] = n
+            if len(n) <= 2 or n.lower() in indian_names:
+                names[i] = n
         other_name_hits = other_name_hits[:5] if len(other_name_hits) >= 5 else other_name_hits
         return names, other_name_hits
 
@@ -139,6 +145,7 @@ class CVReader(object):
             r"(\+91-)?(\+91)?(-\s)?([0-9]{4}).?([0-9]{3}).?([0-9]{3})",
             r"(\+91-)?(\+91)?(-\s)?([0-9]{3}).?([0-9]{4}).?([0-9]{3})",
             r"(\+91-)?(\+91)?(-\s)?([0-9]{4}).?([0-9]{2}).?([0-9]{4})",
+            r"(\+91-)?(\+91)?(-\s)?([0-9]{5}).?([0-9]{5})"
         ]
         for pattern in pattern_mobile:
             matches = re.finditer(pattern, self.text, re.MULTILINE)
@@ -152,7 +159,8 @@ class CVReader(object):
         pattern_email = r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
         matches = re.finditer(pattern_email, self.text, re.MULTILINE)
         for match in matches:
-            emails.add(match.group().lower())
+            email = EmailNormalizer(match.group().lower()).normalize()
+            emails.add(email)
         emails = list(filter(lambda x: x.split(".")[-1] in ("com", "co", "in", "org"), list(emails)))
         emails = emails[:2] if len(emails) >= 2 else emails
         emails = list(set(emails))
@@ -166,6 +174,7 @@ class CVReader(object):
         txt_file_path = self.filename.replace(self.extension, "txt")
         path = os.path.join(CVManager.path_txt_files, txt_file_path)
         with open(path, "w+") as f:
+            self.text_path = path
             f.write(self.text.encode("ascii", "ignore").decode("ascii", "ignore"))
 
         names, others = self.extract_name()
@@ -181,7 +190,7 @@ class CVReader(object):
             "mobile2": mobile2,
             "email1": email1,
             "email2": email2,
-            "name_hints": ", ".join(others)
+            # "name_hints": ", ".join(others)
         }
 
     @classmethod
@@ -190,7 +199,9 @@ class CVReader(object):
         mobiles = list(mobiles)[:2] if len(mobiles) >= 2 else list(mobiles)
         for idx, mobile in enumerate(mobiles):
             norm_mobile = mobile.replace("+91", "").replace(" ", "")
-            mobiles[idx] = int(re.sub(r"\D", "", norm_mobile))
+            norm_mobile = re.sub(r"\D", "", norm_mobile)
+            if norm_mobile[0] in "06789":
+                mobiles[idx] = int(norm_mobile)
         mobiles = list(map(lambda x: int(x), [n for n in map(lambda x: str(x), mobiles) if len(n) == 10]))
         mobiles = list(set(mobiles))
         for idx, number in enumerate(mobiles):
@@ -258,57 +269,125 @@ class CVReader(object):
         return self.extract(text)
 
     def read(self):
+        data = {}
         try:
             if self.extension == CVReader.__doc:
-                return self.read_doc()
+                data = self.read_doc()
             elif self.extension == CVReader.__docx:
-                return self.read_docx()
+                data = self.read_docx()
             elif self.extension == CVReader.__pdf:
-                return self.read_pdf()
+                data = self.read_pdf()
             else:
-                log.info("Skipping file: {}, The file is not in any of the formats({}).".format(
-                    self.path, ", ".join(CVReader.docs_supported)))
-                return {}
+                self.skip = True
+            self.data = data
         except Exception as err:
             log.debug(traceback.format_exc())
             log.error("Error reading the file: {}. Please contact developer to fix it.".format(self.filename))
+        finally:
+            return self
 
 
 class CVManager(object):
 
-    path_txt_files = os.path.join(os.getcwd(), "txts")
-    path_doc2docx_files = os.path.join(os.getcwd(), "doc2docx")
+    output_folder = os.path.join(os.getcwd(), "output")
+    path_txt_files = os.path.join(output_folder, "txts")
+    path_doc2docx_files = os.path.join(output_folder, "doc2docx")
+    path_unread_files = os.path.join(output_folder, "resumes_unread")
+    path_unread_debug_files = os.path.join(path_unread_files, "debug")
 
     def __init__(self, args):
         self.args = args
         self.data = []
+        self.data_unread = []
+        self.stats = {
+            "total": [],
+            "read": [],
+            "unread": [],
+            "skip": []
+        }
+
+    @classmethod
+    def filename(cls, path):
+        return os.path.split(path)[-1]
 
     @classmethod
     def setup(cls):
         os.makedirs(cls.path_txt_files, exist_ok=True)
         os.makedirs(cls.path_doc2docx_files, exist_ok=True)
+        os.makedirs(cls.path_unread_files, exist_ok=True)
+        os.makedirs(cls.path_unread_debug_files, exist_ok=True)
 
     @classmethod
     def cleanup(cls):
-        # shutil.rmtree(CVManager.path_txt_files, ignore_errors=True)
+        shutil.rmtree(CVManager.path_txt_files, ignore_errors=True)
         shutil.rmtree(CVManager.path_doc2docx_files, ignore_errors=True)
+
+    @classmethod
+    def valid(cls, data):
+        if not data["first_name"]:
+            return False
+        if not data["mobile1"]:
+            return False
+        if not data["email1"]:
+            return False
+        return True
 
     def get(self):
         path = os.path.join(self.args.destination, "*")
         paths_all = glob.glob(path, recursive=True)
         paths = list(filter(lambda x: "~$" not in x, paths_all))
 
+        self.stats["total"].extend(paths)
         for path in paths:
-            data = CVReader(path).read()
+            reader = CVReader(path).read()
+            if reader.skip:
+                self.stats["skip"].append(path)
+                continue
+
+            data = reader.data
             log.debug("Fetched data: \n{}".format(json.dumps(data, indent=4)))
             if data:
-                self.data.append(data)
+                if self.valid(data):
+                    self.data.append(data)
+                    self.stats["read"].append(path)
+                else:
+                    self.data_unread.append(data)
+                    self.stats["unread"].append(path)
+
+                    src, dest = path, os.path.join(CVManager.path_unread_files, self.filename(path))
+                    shutil.copyfile(src, dest)
+
+                    src = reader.text_path
+                    dest = os.path.join(CVManager.path_unread_debug_files, self.filename(reader.text_path))
+                    shutil.copyfile(src, dest)
         return self
 
     def save(self):
         df = pd.DataFrame(self.data)
-        path = os.path.join(os.getcwd(), "cv_info.xlsx")
+        path = os.path.join(CVManager.output_folder, "cv_info.xlsx")
         df.to_excel(path, index=False)
+
+        df = pd.DataFrame(self.data_unread)
+        path = os.path.join(CVManager.output_folder, "cv_unread_info.xlsx")
+        df.to_excel(path, index=False)
+
+    def conclude(self):
+        if self.stats["skip"]:
+            log.info("Skipping below files, since the document is not in any of the supported format: {}".format(
+                ", ".join(CVReader.docs_supported)))
+            for path in self.stats["skip"]:
+                log.info("Skipped: {}".format(path))
+
+        total, read, unread, skip = self.stats["total"], self.stats["read"], self.stats["unread"], self.stats["skip"]
+        percent = round((len(read) / (len(total) - len(skip))) * 100, 2)
+        log.info("""File read stats can be found below: 
+        
+        Total files attempted to read: {} 
+        Total files skipped: {}
+        Total files read successfully: {}
+        Total files unread: {}
+        Success: {} %
+        """.format(len(total), len(skip), len(read), len(unread), percent))
 
 
 def get_args():
@@ -328,8 +407,14 @@ def main():
     log.info("Script starts at: {}".format(start))
 
     CVManager.setup()
-    manager = CVManager(args).get()
-    manager.save()
+    manager = CVManager(args)
+    try:
+        manager.get()
+    except BaseException as err:
+        log.error(err)
+    finally:
+        manager.save()
+    manager.conclude()
     CVManager.cleanup()
 
     end = dt.now().strftime("%d-%m-%Y %H:%M:%S %p")
